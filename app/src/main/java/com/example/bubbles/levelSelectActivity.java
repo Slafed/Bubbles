770eb4c3dd4efa84bc0caae835ee73c617b54540
slafe <slafed.dixit01@gmail.com>
2019-10-09 13:36:51 -0500

    yo

---
+++ app/src/main/java/com/example/bubbles/levelSelectActivity.java
@@ -0,0 +1,14 @@
+package com.example.bubbles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class levelSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_select);
+    }
+}
