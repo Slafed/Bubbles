00d8506bd54e7c203cefd762fa485515f2d736f0
kovathana5608 <kovathana5608@students.d211.org>
2019-10-17 13:18:04 -0500

    Circle Locked in place

---
+++ app/src/main/java/com/example/bubbles/Bubble.java
@@ -0,0 +1,21 @@
+package com.example.bubbles;
+
+import android.graphics.Paint;
+
+public class Bubble {
+    private float x;
+    private float y;
+    final float radius = 20;
+
+    public Bubble(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+/*
+    public void drawCirle() {
+        drawCirle(, , , );
+    }
+*/
+
+}
