190d766fa4f60f3b25efc9ff2f424a06bc138b6a
imaboss90 <54406879+imaboss90@users.noreply.github.com>
2019-10-18 13:48:37 -0500

    pop up

--- app/src/main/java/com/example/bubbles/MainActivity.java
+++ app/src/main/java/com/example/bubbles/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LevelActivity.class);
 
         startActivity(intent);
+
+        Button b = (Button) findViewById(R.id.tutorial);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Denna", "inside setOnClickListener");
+                startActivity(new Intent(MainActivity.this,Pop.class));
+
+            }
+        });
     }
 
     public void campaign(View v){
---
+++ app/src/main/java/com/example/bubbles/Pop.java
@@ -0,0 +1,29 @@
+package com.example.bubbles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+
+public class Pop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop);
+
+        Log.d("Denna", "inside Pop onCreate");
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        Log.d("Denna", "inside Pop onCreate: width: " + width + " height: " + height);
+        getWindow().setLayout((int)(width*.8), (int)(height*.6));
+
+    }
+}
