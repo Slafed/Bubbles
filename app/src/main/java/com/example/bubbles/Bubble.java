2c0ddd56997acd3b2ec76146c975c3937b7c0997
kovathana5608 <kovathana5608@students.d211.org>
2019-10-21 13:49:43 -0500

    Bubbles Array Moving Working

--- app/src/main/java/com/example/bubbles/Bubble.java
+++ app/src/main/java/com/example/bubbles/Bubble.java
@@ -5,17 +5,38 @@ import android.graphics.Paint;
 public class Bubble {
     private float x;
     private float y;
-    final float radius = 20;
+    final float radius = 100;
 
     public Bubble(float x, float y)
     {
         this.x = x;
         this.y = y;
     }
-/*
-    public void drawCirle() {
-        drawCirle(, , , );
+
+    public float getY() {
+        return y;
     }
-*/
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void moveX(float x)
+    {
+        this.x += x;
+    }
+    public void moveY()
+    {
+        this.y -=10;
+    }
+
 
 }
