d01317d2abd225b148ef5a1fbab5c811b7aea11f
Ahmer <deadrat7078@gmail.com>
2019-10-24 05:54:06 -0500

    multi level start

--- app/src/main/java/com/example/bubbles/LevelSelectActivity.java
+++ app/src/main/java/com/example/bubbles/LevelSelectActivity.java
@@ -16,13 +16,42 @@ public class LevelSelectActivity extends AppCompatActivity {
 
     public void selectedLevel(View v)
     {
-        /*if(v =  findViewById("button1"))
+        if(v == findViewById(R.id.button1))
         {
+            String num ="1";
+            Intent intent = new Intent(this, LevelActivity.class);
+            intent.putExtra(LevelActivity.SELECTED_LEVEL, num) ;
+            startActivity(intent);
+        }
+        else if(v == findViewById(R.id.button2))
+        {
+            String num ="2";
+            Intent intent = new Intent(this, LevelActivity.class);
+            intent.putExtra(LevelActivity.SELECTED_LEVEL, num) ;
+            startActivity(intent);
+        }
+        else if(v == findViewById(R.id.button3))
+        {
+            String num ="3";
+            Intent intent = new Intent(this, LevelActivity.class);
+            intent.putExtra(LevelActivity.SELECTED_LEVEL, num) ;
+            startActivity(intent);
+        }
+        else if(v == findViewById(R.id.button4))
+        {
+            String num ="4";
+            Intent intent = new Intent(this, LevelActivity.class);
+            intent.putExtra(LevelActivity.SELECTED_LEVEL, num) ;
+            startActivity(intent);
+        }
+        else if(v == findViewById(R.id.button5))
+        {
+            String num ="5";
+            Intent intent = new Intent(this, LevelActivity.class);
+            intent.putExtra(LevelActivity.SELECTED_LEVEL, num) ;
+            startActivity(intent);
+        }
 
-        }*/
-        Intent intent = new Intent(this, LevelActivity.class);
-
-        startActivity(intent);
 
     }
 
